4965d80d40d23e80167b0d8b1da74fb44e0bcb73
Ubuntu <AzureUser@linux1.pslshn1xgnbufanznyxgyelb0a.hx.internal.cloudapp.net>
2020-06-07 04:09:18 +0000

    update.

--- shadowsocks_install.sh
+++ shadowsocks_install.sh
@@ -1,4 +1,6 @@
-sudo apt-get update
-sudo apt-get install python-pip
-pip install setuptools
-pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+#sudo apt-get install python-pip
+#pip install setuptools
+#pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+
+sudo apt update
+sudo apt install shadowsocks
